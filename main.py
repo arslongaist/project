08f1bffed357f64cabe3ecc4e7c2c6001dabc245
Furkan -arslongaist- Demir <106020247+arslongaist@users.noreply.github.com>
2022-11-05 19:54:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 y0 = [0, 0]
 t = np.linspace(0, 10, num=5000)
 
-
+#varsayılan değerler
 lambda_1 = 10
 gama_1 = 1
 lambda_2 = 100
@@ -16,6 +16,12 @@ gama_2 = 2
 
 n = 1
 c = 1
+
+#olağan dışı değişken örneği
+#lambda_1 = 10
+#gama_1 = 3
+#lambda_2 = 100
+#gama_2 = 1
 
 # n ve c sabit değerlerdir. Hill fonksiyonu tanımlanmasında katsayı olarak kullanılır.
 # lambda_1 değişkeni bu modelde kullanılan Gen 1'in ifade edilme oranını gösterir.
